f3c59b6db47e2ad05c5132a0d8d72571b06e12be
Rodrigo Rocha <117938747+RodrigoRocS@users.noreply.github.com>
2025-10-27 12:26:35 -0300

    NIVEL NOVATO DESAFIO WAR

--- war.c
+++ war.c
@@ -31,6 +31,30 @@
 
 // --- Função Principal (main) ---
 // Função principal que orquestra o fluxo do jogo, chamando as outras funções em ordem.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define struct do territorio
+
+typedef struct
+{
+    char nome[30], cor[10];
+    int tropas;
+} Territorio;
+
+Territorio territorios[5]; // vetor com todos os territorios
+int numTerritorio = 0;     // contador de territorios
+
+// Limpa buffer de entrada
+
+void limparBufferEntrada()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+}
+
 int main() {
     // 1. Configuração Inicial (Setup):
     // - Define o locale para português.
@@ -50,6 +74,37 @@ int main() {
 
     // 3. Limpeza:
     // - Ao final do jogo, libera a memória alocada para o mapa para evitar vazamentos de memória.
+
+    printf("Vamos cadastrar os 5 territorios iniciais do nosso mundo: \n");
+    do // inicia cadastro dos territorios
+    {
+        printf("\n--- Cadastrando Territorio %d ---\n", numTerritorio + 1);
+
+        printf("Nome do territorio: ");
+        fgets(territorios[numTerritorio].nome, 30, stdin);
+
+        printf("Cor do territorio: ");
+        fgets(territorios[numTerritorio].cor, 10, stdin);
+
+        printf("Numero de tropas: ");
+        scanf("%d", &territorios[numTerritorio].tropas);
+
+        limparBufferEntrada();
+
+        numTerritorio++; // incrementa o loop
+
+    } while (numTerritorio < 5);
+
+    // exibe territorios cadastrados
+
+    printf("\n=== Territorios cadastrados ===\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\n--- Territorio %d ---\n", i + 1);
+        printf("Nome: %s", territorios[i].nome);
+        printf("Cor: %s", territorios[i].cor);
+        printf("Tropas: %d\n", territorios[i].tropas);
+    }
 
     return 0;
 }
